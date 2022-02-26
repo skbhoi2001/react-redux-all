1bc65df1f232897a768f8a2bbd921688cad37514
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-02-26 10:38:04 +0530

    mw

--- redux/src/store/auth/auth.reducer.js
+++ redux/src/store/auth/auth.reducer.js
@@ -1,11 +1,12 @@
 import { updateValue } from "../../utils/localStorage";
 import { USER_LOGIN, USER_LOGOUT } from "./auth.actionTypes";
 import { initialState } from "./auth.constants";
+import {v4 as uuid} from "uuid"
 
 export const authReducer = (state = initialState, { type }) => {
   switch (type) {
     case USER_LOGIN: {
-      let userToken = "ABCD";
+      let userToken = uuid()
       updateValue("userToken", userToken);
       return {
         ...state,
